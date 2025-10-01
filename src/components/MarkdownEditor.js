160960e7d14ab3865f1858aace560b58e8d58a02
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-01 21:17:03 +0530

    Update MarkdownEditor.js

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,1 +1,33 @@
+import React,{useState,useEffect} from 'react';
+import ReactMarkdown from 'react-markdown'
 
+function MarkdownEditor(){
+  const [text,setText] = useState("");
+  const [preview,setPreview] = useState("");
+
+  useEffect(()=>{
+    setPreview(text);
+  },[text]);
+
+  return(
+    <div className="markdown-editor">
+        <div className="input-section">
+            <h2>Markdown Input</h2>
+            <textarea
+            className="textarea"
+            value={text}
+            onChange={(e)=>setText(e.target.value)}
+            placeholder="Enter Markdown here..."
+              />
+        </div>
+        <div className="preview-section">
+            <h2>Preview</h2>
+            <div className="preview">
+                <ReactMarkdown>{preview}</ReactMarkdown>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MarkdownEditor;
